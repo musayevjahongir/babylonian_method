d07fea16381fdce6a09554f27734518cec25e15b
musayevjahongir <musayevjahongir5051@gmail.com>
2023-09-26 09:52:41 +0500

    hisoblandi

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ def main(S, d):
     Returns:
         float: result
     '''
-    a=(S-d**2)/2*d
+    a=(S-d**2)/(2*d)
     b=a+d
-    x=b-a**2/2*b
+    x=b-(a**2)/(2*b)
     return x
-print(main(16, 4))
+print(main(26, 5))
